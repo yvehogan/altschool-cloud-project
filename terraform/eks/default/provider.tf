f5a99507fdaa111851338c22823cc407e3502bf0
yvehogan <mz.yve3@gmail.com>
2025-10-22 20:11:21 +0100

    initial commit for cloud school github action setup

---
+++ terraform/eks/default/provider.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~> 1.19.0"
+    }
+  }
+}
